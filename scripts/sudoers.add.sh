9ceba30976d1de7411df71f5175e0585d259f132
Sergey Dzyuban <sergey.dziuban@intapp.com>
2022-05-17 13:58:42 +0300

    fix

--- scripts/sudoers.add.sh
+++ scripts/sudoers.add.sh
@@ -1,3 +1,2 @@
 usermod -aG sudo cloud_user 
-echo "cloud_user  ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers
-echo "%sudo  ALL=(ALL:ALL) ALL" > /etc/sudoers
+echo "cloud_user  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
